d7306dceb17e479a99a908782d6dbda279b7d7bc
Paul Moros Anacona <pmoros@outlook.com>
2023-10-10 22:51:14 -0500

    fix: change cli interface

--- src/main.c
+++ src/main.c
@@ -1,49 +1,46 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
 
-// Function to display help message
-void print_help() {
-    printf("Usage: jgs <command> [options] <file_path>\n");
-    printf("Commands:\n");
-    printf("  describe --levels <n> <file_path> : Describe a file\n");
-    printf("  sum <field.subfield> <file_path>  : Calculate the sum\n");
-    printf("  mean <field.subfield> <file_path> : Calculate the mean\n");
-    printf("  median <field.subfield> <file_path> : Calculate the median\n");
-    printf("  count <field.subfield> <file_path> : Count items\n");
+void printHelp() {
+    printf("Description: A tool for finding the best path through a graph in a JSON file.\n");
+    printf("Usage: jgs [OPTIONS] FILE_PATH\n");    
+    printf("  --framework   Specify the framework (e.g., OpenMP, CUDA, MPI)\n");
+    printf("  -h, --help    Display this help message\n");
 }
 
 int main(int argc, char *argv[]) {
-    if (argc < 3) {
-        printf("Missing command or file path.\n");
-        print_help();
+    char *framework = NULL;
+    char *file_path = NULL;
+
+    // Parse command line arguments
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "--framework") == 0 && i + 1 < argc) {
+            framework = argv[i + 1];
+            i++; // Skip the next argument as it's the framework value
+        } else if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0) {
+            printHelp();
+            return 0;
+        } else {
+            // Assuming the last non-flag argument is the file path
+            file_path = argv[i];
+        }
+    }
+
+    // Check if a file path was provided
+    if (file_path == NULL) {
+        printf("Error: Please provide a FILE_PATH.\n");
+        printHelp();
         return 1;
     }
 
-    char *command = argv[1];
-    const char *file_path = argv[argc - 1];
+    // Your code to use the 'framework' and 'file_path' variables goes here
 
-    if (strcmp(command, "describe") == 0) {
-        if (argc != 6 || strcmp(argv[2], "--levels") != 0) {
-            printf("Invalid 'describe' command. Use 'jgs describe --levels <n> <file_path>'.\n");
-            return 1;
-        }
-        int levels = atoi(argv[3]);
-        // Implement 'describe' logic
-        printf("Running 'describe' with levels: %d, file path: %s\n", levels, file_path);
-    } else if (strcmp(command, "sum") == 0 || strcmp(command, "mean") == 0 || strcmp(command, "median") == 0 || strcmp(command, "count") == 0) {
-        if (argc != 4) {
-            printf("Invalid '%s' command. Use 'jgs %s <field.subfield> <file_path>'.\n", command, command);
-            return 1;
-        }
-        const char *field = argv[2];
-        // Implement logic for sum, mean, median, and count
-        printf("Running '%s' for field: %s, file path: %s\n", command, field, file_path);
+    if (framework) {
+        printf("Framework: %s\n", framework);
     } else {
-        printf("Unknown command: %s\n", command);
-        print_help();
-        return 1;
+        printf("No framework specified.\n");
     }
+    printf("File Path: %s\n", file_path);
 
     return 0;
 }
